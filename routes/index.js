81064da8be1791b8c85845c08a2a374f64d7ed67
Jooyong Sung <camon85@gmail.com>
2015-09-21 14:54:52 +0900

    google oauth 연동

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'camonyo' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'camonyo' });
+});
+
+router.post('/login-process', function(req, res, next) {
+  var id = req.param('id');
+  var email = req.param('email');
+  var nickName = req.param('nickName');
+  console.log('#id: ' + id);
+
+  // TODO DB에 insert
+
+  res.redirect("/");
+});
+
 router.get('/maps/:id', function(req, res, next) {
   var id = req.param('id');
   res.render('map', { mapId: id });
